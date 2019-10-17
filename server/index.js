4ad2d0dc9d0be48a9d3cb8f0eff741b3808cac5c
Corned <oliver@kaarnalehto.fi>
2019-10-17 14:33:19 +0200

    port change

--- server/index.js
+++ server/index.js
@@ -4,5 +4,5 @@ const http = require("http")
 const app = express()
 const server = http.Server(app)
 
-const PORT = process.env.PORT || 8080
-server.listen(PORT, () => console.log(`Server running on port ${8080}`))
+const PORT = process.env.PORT || 80
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
