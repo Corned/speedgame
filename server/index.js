4a7861a0cd72937cebadcba942fc65b1eaa12ffa
Corned <oliver@kaarnalehto.fi>
2019-10-17 17:56:45 +0200

    server serves app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const http = require("http")
+const path = require("path")
 
 const app = express()
 const server = http.Server(app)
+
+app.use(express.static(path.join(__dirname, "../build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../build", "index.html")
+  )
+})
 
 const PORT = process.env.PORT || 80
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
