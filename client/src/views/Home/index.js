2fdaeccd05e6a520a9d291e2e290f95222bd6448
Corned <oliver@kaarnalehto.fi>
2019-10-23 22:14:40 +0300

    home view

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,14 +1,28 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import { Redirect, useHistory } from "react-router-dom"
 
-import "index.scss"
+import "./index.scss"
 
 const Home = () => {
+  const [ redirect, setRedirect ] = useState(false)
+  const history = useHistory()
+
+  const handleRedirect = () => setRedirect(true)
+
+  if (redirect) {
+    history.push("/")
+    return (
+      <Redirect to="/play"/>
+    )
+  }
+
   return (
     <>
-      <h1>Speed Game</h1>
-      <p>highscore: 40</p>
-      <Link to="/play">start</Link>
+      <h1 className="home__header--large">Speed Game</h1>
+      <p className="home__highscore">highscore: 40</p>
+      <button onClick={handleRedirect} className="home__start-game">
+        start
+      </button>
     </>
   )
 }
